94ac57fb715c7a6cce6f355c9a125cb81f3cfa2e
Alfredo Fanghella <alfredojfv.94@gmail.com>
2016-09-21 17:42:54 +0200

    Added theoretical values to HMM test

--- PattRecClasses/Verification3.m
+++ PattRecClasses/Verification3.m
@@ -16,3 +16,6 @@ g2 = GaussD('Mean', 3, 'StDev', 2); %Distribution for state=2
 h = HMM(mc, [g1; g2]); %The HMM
 x = rand(h, 10000); %Generate an output sequence
 display(sprintf('Mean: %f\nVariance: %f', mean(x), var(x)))
+% Theoretical values are:
+% Mean: 0.75
+% Variance: 3.4365
