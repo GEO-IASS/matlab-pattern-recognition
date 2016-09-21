a0fb056aeb7d444670960129a1b64ae983c58254
Alfredo Fanghella <alfredojfv.94@gmail.com>
2016-09-21 23:21:28 +0200

    Added display of numer of states to finite HMM test

--- PattRecClasses/Verification6.m
+++ PattRecClasses/Verification6.m
@@ -14,9 +14,10 @@ mc = MarkovChain([0.75; 0.25], [0.98 0.01 0.01; 0.03 0.96 0.01]);
 g1 = GaussD('Mean', 0, 'StDev', 1); %Distribution for state=1
 g2 = GaussD('Mean', 3, 'StDev', 2); %Distribution for state=2
 h = HMM(mc, [g1; g2]); %The HMM
-x = rand(h, 500); %Generate an output sequence
+[x s] = rand(h, 500); %Generate an output sequence
 
 plot(x)
 title('HMM output')
 xlabel('time(t)')
 ylabel('Samples')
+display(sprintf('Number of states: %d', length(s)));
